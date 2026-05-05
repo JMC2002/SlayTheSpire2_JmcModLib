5165e25b1fc9ea4d3956bd45fb7d698a96dc1fd9
MC J <jmc8@qq.com>
2026-05-06 03:44:48 +0800

    增加许可证信息

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -6,7 +6,7 @@ namespace JmcModLib.Core;
 public static class VersionInfo
 {
     public const string Name = "JmcModLib";
-    public const string Version = "1.0.96";
+    public const string Version = "1.0.98";
 
     public static string Tag => $"[{Name} v{Version}]";
 
